5c2053c1dcf565102cc63b2eca2840c1da4db994
Flávio Eduardo <xflax00@gmail.com>
2025-03-12 11:11:12 -0300

    chore(alias): Implementing alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
     host: true,
-  }
-})
+  },
+  resolve: {
+    alias: {
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@components": path.resolve(__dirname, "./src/shared/components"),
+      "@icons": path.resolve(__dirname, "./src/shared/icons"),
+    },
+  },
+});
